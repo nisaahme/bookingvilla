a85782133c7ef9cb0ece63e78e0946f9ed943992
RuvejdaHamza <ruvejda.hamza@student.uni-pr.edu>
2026-04-13 13:58:12 -0200

    Update villa.php

--- classes/villa.php
+++ classes/villa.php
@@ -37,7 +37,7 @@ class Villa {
     public static function getAll() {
         $villas = [   // <--- KRIJO NDRYSHOREN $villas
             new Villa(1, 'Seaside Paradise', 'Sarandë, Albania', 124, 'foto1.jpg'),
-            new Villa(2, 'Mountain Retreat', 'Theth, Albania', 90, 'foto2.jpg'),
+            new Villa(2, 'Mountain Retreat', 'Theth, Albania', 95, 'foto2.jpg'),
             new Villa(3, 'Lake View Villa', 'Ohrid, North Macedonia', 150, 'foto3.jpg'),
             new Villa(4, 'Olive Grove Estate', 'Berat, Albania', 110, 'foto4.jpg'),
             new Villa(5, 'Sunset Cliff Villa', 'Himare, Albania', 180, 'foto5.jpg'),
