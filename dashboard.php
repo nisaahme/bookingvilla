a9da469b3b242dc5d8d69b6c871c5e8b97e33803
RuvejdaHamza <ruvejda.hamza@student.uni-pr.edu>
2026-04-12 22:26:17 -0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@ require_once 'includes/nav.php';
 <h2>My Bookings</h2>
 
 <?php if (empty($myBookings)): ?>
-    <p>You have no bookings yet. <a href="villas.php">Book a villa now!</a></p>
+    <p>Nuk keni rezervime të regjistruara. <a href="villas.php">Rezervoni një villë tani!</a></p>
 <?php else: ?>
     <table border="1" cellpadding="10">
         <tr>
