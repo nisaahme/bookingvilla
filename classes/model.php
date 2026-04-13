3e69487fd41db027d920f021deb82c57ec3ba16d
RuvejdaHamza <ruvejda.hamza@student.uni-pr.edu>
2026-04-13 13:46:46 -0200

    Update model.php

--- classes/model.php
+++ classes/model.php
@@ -1,1 +1,18 @@
+<?php
+class Model {
+    protected $id;
+    protected $created_at;
 
+    public function __construct() {
+        $this->created_at = date('Y-m-d H:i:s');
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getCreatedAt() {
+        return $this->created_at;
+    }
+}
+?>
