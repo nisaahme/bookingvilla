a63b314d791d7312e92a06658c7ba09665152370
DenisaIsufi <denisa.isufi@student.uni-pr.edu>
2026-04-16 13:22:27 +0200

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,1 +1,18 @@
+<?php
+require_once 'classes/Villa.php';
+require_once 'classes/Booking.php';
+require_once 'includes/auth.php';
 
+requireLogin();
+
+$villa_id = $_GET['villa_id'] ?? null;
+$villa = null;
+
+// Gjej vilën përkatëse
+$allVillas = Villa::getAll();
+foreach ($allVillas as $v) {
+    if ($v->getId() == $villa_id) {
+        $villa = $v;
+        break;
+    }
+} ?>
